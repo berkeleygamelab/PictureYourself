7ce29dd8f1f536ba9be7e3e8de2b5ab2dabd62bb
Andrew Dorsett <awdorsett@berkeley.edu>
2013-10-14 14:47:38 -0700

    added test3 to root, changed picture_yourself_system to reference test3

--- picture_yourself_system.rb
+++ picture_yourself_system.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 
 get '/' do
   system('mkfifo pipename.pipe')
-  system('nohup programname < pipename.pipe &
+  system('nohup ./test3 test.jpg 10 10 200 200 < pipename.pipe &
   echo command > pipename.pipe')
   erb :index
 end
