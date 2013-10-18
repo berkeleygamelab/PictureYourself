99b4834afd418dd87df593731e8781edbd4890c5
Andrew Dorsett <awdorsett@berkeley.edu>
2013-10-18 13:33:17 -0700

    added upload, preview of picture, and selection square

--- public/js/PictureYourself.js
+++ public/js/PictureYourself.js
@@ -1,106 +1,105 @@
-app = angular.module('PictureYourselfApp', ['ngResource'])
+var UploadController = function ($scope, fileReader) {
+     console.log(fileReader)
+    $scope.getFile = function () {
+        $scope.progress = 0;
+        fileReader.readAsDataUrl($scope.file, $scope)
+                      .then(function(result) {
+                        $scope.imageSrc = result;
+				      	var imageObj = new Image();
+						imageObj.src = result;
+						
+						var stage = new Kinetic.Stage({
+				        container: 'container',
+				        width: imageObj.width,
+				        height: imageObj.height
+				      	});
+						
+						var background = new Kinetic.Rect({
+							x:0,
+							y:0,
+							width: imageObj.width,
+							height: imageObj.height,
+							fillEnabled: false,
+							opacity: 1
+						});
+						
+						var selection = new Kinetic.Rect({
+							x:0,
+							y:0,
+							stroke:'yellow',
+							strokeWidth: 3,
+							fillEnabled: false
+						})
+						var originalPoint = {x: selection.getX(), y: selection.getY()};
+				
+				      	var layer = new Kinetic.Layer();
+						var down = false;
+						
+				      	imageObj.onload = function() {
+				        	var yoda = new Kinetic.Image({
+				          		x: 0,
+				          		y: 0,
+				          		image: imageObj,
+				          		width: imageObj.width,
+				          		height: imageObj.height
+				        	});
 
-FileUploadCtrl.$inject = ['$scope']
-function FileUploadCtrl(scope) {
-    //============== DRAG & DROP =============
-    // source for drag&drop: http://www.webappers.com/2011/09/28/drag-drop-file-upload-with-html5-javascript/
-    var dropbox = document.getElementById("dropbox")
-    scope.dropText = 'Drop files here...'
+				       	 // add the shape to the layer
+					        layer.add(yoda);
+							layer.add(background);
+							layer.add(selection);
 
-    // init event handlers
-    function dragEnterLeave(evt) {
-        evt.stopPropagation()
-        evt.preventDefault()
-        scope.$apply(function(){
-            scope.dropText = 'Drop files here...'
-            scope.dropClass = ''
-        })
+				       	 // add the layer to the stage
+				       	 stage.add(layer);
+				      	}; // end of imageObj.onload
+					
+					stage.on('mousedown',function(){
+						down = true;
+						selection.setX(stage.getMousePosition().x);
+						selection.setY(stage.getMousePosition().y);
+					})
+					
+					stage.on('mouseup', function(){
+						down = false;
+					})
+
+					stage.on('mousemove', function(){
+						if (!down) return;
+
+						selection.setWidth(stage.getMousePosition().x - selection.getX());
+						selection.setHeight(stage.getMousePosition().y - selection.getY());
+						layer.draw();
+					})
+
+					}); //end of then
+ 
+					
+    $scope.$on("fileProgress", function(e, progress) {
+        $scope.progress = progress.loaded / progress.total;
+    });
+ 
+}; //$scope.getFile
+} //UploadController
+
+app.directive("ngFileSelect",function(){
+
+  return {
+    link: function($scope,el){
+      
+      el.bind("change", function(e){
+      
+        $scope.file = (e.srcElement || e.target).files[0];
+        $scope.getFile();
+      })
+      
     }
-    dropbox.addEventListener("dragenter", dragEnterLeave, false)
-    dropbox.addEventListener("dragleave", dragEnterLeave, false)
-    dropbox.addEventListener("dragover", function(evt) {
-        evt.stopPropagation()
-        evt.preventDefault()
-        var clazz = 'not-available'
-        var ok = evt.dataTransfer && evt.dataTransfer.types && evt.dataTransfer.types.indexOf('Files') >= 0
-        scope.$apply(function(){
-            scope.dropText = ok ? 'Drop files here...' : 'Only files are allowed!'
-            scope.dropClass = ok ? 'over' : 'not-available'
-        })
-    }, false)
-    dropbox.addEventListener("drop", function(evt) {
-        console.log('drop evt:', JSON.parse(JSON.stringify(evt.dataTransfer)))
-        evt.stopPropagation()
-        evt.preventDefault()
-        scope.$apply(function(){
-            scope.dropText = 'Drop files here...'
-            scope.dropClass = ''
-        })
-        var files = evt.dataTransfer.files
-        if (files.length > 0) {
-            scope.$apply(function(){
-                scope.files = []
-                for (var i = 0; i < files.length; i++) {
-                    scope.files.push(files[i])
-                }
-            })
-        }
-    }, false)
-    //============== DRAG & DROP =============
+    
+  }
+  
+  
+})
 
-    scope.setFiles = function(element) {
-    scope.$apply(function(scope) {
-      console.log('files:', element.files);
-      // Turn the FileList object into an Array
-        scope.files = []
-        for (var i = 0; i < element.files.length; i++) {
-          scope.files.push(element.files[i])
-        }
-      scope.progressVisible = false
-      });
-    };
 
-    scope.uploadFile = function() {
-        var fd = new FormData()
-        for (var i in scope.files) {
-            fd.append("uploadedFile", scope.files[i])
-        }
-        var xhr = new XMLHttpRequest()
-        xhr.upload.addEventListener("progress", uploadProgress, false)
-        xhr.addEventListener("load", uploadComplete, false)
-        xhr.addEventListener("error", uploadFailed, false)
-        xhr.addEventListener("abort", uploadCanceled, false)
-        xhr.open("POST", "/fileupload")
-        scope.progressVisible = true
-        xhr.send(fd)
-    }
-
-    function uploadProgress(evt) {
-        scope.$apply(function(){
-            if (evt.lengthComputable) {
-                scope.progress = Math.round(evt.loaded * 100 / evt.total)
-            } else {
-                scope.progress = 'unable to compute'
-            }
-        })
-    }
-
-    function uploadComplete(evt) {
-        /* This event is raised when the server send back a response */
-        alert(evt.target.responseText)
-    }
-
-    function uploadFailed(evt) {
-        alert("There was an error attempting to upload the file.")
-    }
-
-    function uploadCanceled(evt) {
-        scope.$apply(function(){
-            scope.progressVisible = false
-        })
-        alert("The upload has been canceled by the user or the browser dropped the connection.")
-    }
-}
 
 function LayoutCtrl($scope){
 	
