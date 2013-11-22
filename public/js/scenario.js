65ff95f0d8a12697c0ed62de4ac5f98042c6af9f
Alex Liu <alex_liu@berkeley.edu>
2013-11-21 19:35:19 -0800

    data about stickers on canvas saved into stickers array

--- public/js/scenario.js
+++ public/js/scenario.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //http://angular-ui.github.io/bootstrap/
 
 window.mynamespace = window.mynamespace || {};
@@ -56,10 +56,13 @@ $('.background').click(function(){
 
 
 
+//every time a sticker is added to the stage, also add it to a global array for session storing
 /*
 * The actual Kinetic Stage where users can move stickers around
 */
-window.onload = function(){
+
+//try to wrap this all in a function somehow? ScenarioCtrl is preventing that from happening
+    var stickers = []; //will store information about stickers
     var stage = new Kinetic.Stage({
       container: 'container',
       width: parseInt($('#container').css('width')) ,
@@ -69,17 +72,26 @@ window.onload = function(){
     stage.add(layer);
     var con = stage.getContainer();
     var dragSrcEl = null;
-    //image
-    $('.sticker').bind('dragstart',function(e){  //!!!!!ALL STICKERS MUST HAVE CLASS 'sticker'
-           dragSrcEl = this;
-    });
 
+    function ScenarioCtrl($scope, $resource, $http){
+      $http.get('test/stickers').success(function(data){
+        angular.forEach(data,function(sticker){
+           console.log(sticker)
+           if(sticker.match('txt') == null)
+              $("#tab1").append('<img class=\'sticker\' src="/' + sticker + '">');
+        }); //forEach
+        //image
+        $('.sticker').bind('dragstart',function(e){  //!!!!!ALL STICKERS MUST HAVE CLASS 'sticker'
+         dragSrcEl = this;
+        });
+
+    });//success
+}
     con.addEventListener('dragover',function(e){
         e.preventDefault(); //@important
     });
     //insert image to stage
     con.addEventListener('drop',function(e){
-        console.log(e);
         var image = new Kinetic.Image({
            draggable : true,
            width: 120,  //this makes the image lower quality for some reason
@@ -89,6 +101,7 @@ window.onload = function(){
         });
         layer.add(image);
         imageObj = new Image();
+
         imageObj.src = dragSrcEl.src;
         imageObj.onload = function(){
             image.setImage(imageObj)
@@ -98,12 +111,19 @@ window.onload = function(){
      });
 
 
-
-
-
-}
-
-
+//layer.getChildren()[0].attrs.image.src
+//layer.getChildren()[0].attrs.x
+$(window).on('click', function(){
+  $.each(layer.getChildren(), function(index, value){
+    var sticker = {
+      src: value.attrs.image.src,
+      x: value.attrs.x,
+      y: value.attrs.y
+    }
+    stickers.push(sticker);
+  })
+  console.log(stickers)
+});
 
 //http://jsfiddle.net/projeqht/ttUd4/
 
@@ -121,65 +141,65 @@ window.onload = function(){
 
 
 //http://superdit.com/2011/12/04/jquery-plugin-for-rotating-image/
-;(function( $ ) {
-    $.fn.myrotate = function() {
-        var img = this.find("img"); //wtf
-        var imgpos = img.position();
-        var x0, y0;
+// ;(function( $ ) {
+//     $.fn.myrotate = function() {
+//         var img = this.find("img"); //wtf
+//         var imgpos = img.position();
+//         var x0, y0;
 
-        $(window).load(function() {
-            img.removeAttr("width");
-            img.removeAttr("height");
+//         $(window).load(function() {
+//             img.removeAttr("width");
+//             img.removeAttr("height");
 
-            x0 = imgpos.left + (img.width() / 2);
-            y0 = imgpos.top + (img.height() / 2);
-        });
+//             x0 = imgpos.left + (img.width() / 2);
+//             y0 = imgpos.top + (img.height() / 2);
+//         });
 
-        var x, y, x1, y1, drag = 0;
+//         var x, y, x1, y1, drag = 0;
 
-        img.css({
-            "cursor": "pointer",
-            "position": "relative"
-        });
+//         img.css({
+//             "cursor": "pointer",
+//             "position": "relative"
+//         });
 
-        img.mousemove(function(e) {
-            x1 = e.pageX;
-            y1 = e.pageY;
-            x = x1 - x0;
-            y = y1 - y0;
+//         img.mousemove(function(e) {
+//             x1 = e.pageX;
+//             y1 = e.pageY;
+//             x = x1 - x0;
+//             y = y1 - y0;
 
-            r = 360 - ((180/Math.PI) * Math.atan2(y,x));
+//             r = 360 - ((180/Math.PI) * Math.atan2(y,x));
 
-            if (drag == 1) {
-                img.css("transform","rotate(-"+r+"deg)");
-                img.css("-moz-transform","rotate(-"+r+"deg)");
-                img.css("-webkit-transform","rotate(-"+r+"deg)");
-                img.css("-o-transform","rotate(-"+r+"deg)");
-            }
-        });
+//             if (drag == 1) {
+//                 img.css("transform","rotate(-"+r+"deg)");
+//                 img.css("-moz-transform","rotate(-"+r+"deg)");
+//                 img.css("-webkit-transform","rotate(-"+r+"deg)");
+//                 img.css("-o-transform","rotate(-"+r+"deg)");
+//             }
+//         });
 
-        img.mousedown(function() {
-            if (drag == 0) {
-                drag = 1;
-                img.css("-webkit-box-shadow", "0 0 5px #999");
-                img.css("-moz-box-shadow", "0 0 5px #999");
-                img.css("box-shadow", "0 0 5px #999");
-            } else {
-                drag = 0;
-                img.css("-webkit-box-shadow", "0 0 2px #999");
-                img.css("-moz-box-shadow", "0 0 2px #999");
-                img.css("box-shadow", "0 0 2px #999");
-            }
-        });
+//         img.mousedown(function() {
+//             if (drag == 0) {
+//                 drag = 1;
+//                 img.css("-webkit-box-shadow", "0 0 5px #999");
+//                 img.css("-moz-box-shadow", "0 0 5px #999");
+//                 img.css("box-shadow", "0 0 5px #999");
+//             } else {
+//                 drag = 0;
+//                 img.css("-webkit-box-shadow", "0 0 2px #999");
+//                 img.css("-moz-box-shadow", "0 0 2px #999");
+//                 img.css("box-shadow", "0 0 2px #999");
+//             }
+//         });
 
-        img.mouseleave(function() {
-            drag = 0;
-        });
-    };
-})( jQuery );
+//         img.mouseleave(function() {
+//             drag = 0;
+//         });
+//     };
+// })( jQuery );
 
 
-$('.sticker').myrotate();
+// $('.sticker').myrotate();
 
 
 
@@ -234,36 +254,9 @@ $(window).on('beforeunload', function(){
   xhr2.send(formData);
 });
 
-function ScenarioCtrl($scope, $resource, $http){
-
-		var layer = new Kinetic.Layer();
-		var stage = new Kinetic.Stage({
-		    container: 'container',
-		    width: 600,
-		    height: 600
-		});
-		stage.add(layer);
-
-		$http.get('test/stickers').success(function(data){
-			angular.forEach(data,function(sticker){
-
-				var image = new Image();
-				image.src = sticker;
-
-				image.onload = function() {
-					var pic = new Kinetic.Image({
-						image:image,
-						draggable:true
-					});
-
-					layer.add(pic);
-					layer.draw();
-				};
-
-			}); //forEach
-		});//success
 
 
 
 
-}
+
+
