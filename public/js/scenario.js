f8313d624c44421b1dadb5dd0cbd44da455308bf
Andrew Dorsett <awdorsett@berkeley.edu>
2013-12-05 18:26:54 -0800

    update

--- public/js/scenario.js
+++ public/js/scenario.js
@@ -4,33 +4,33 @@
 * Tabs
 */
 
-;(function($){
-    $.fn.html5jTabs = function(options){
-        return this.each(function(index, value){
-            var obj = $(this),
-            objFirst = obj.eq(index),
-            objNotFirst = obj.not(objFirst);
+// ;(function($){
+//     $.fn.html5jTabs = function(options){
+//         return this.each(function(index, value){
+//             var obj = $(this),
+//             objFirst = obj.eq(index),
+//             objNotFirst = obj.not(objFirst);
 
-            $("#" +  objNotFirst.attr("data-toggle")).hide();
-            $(this).eq(index).addClass("active");
+//             $("#" +  objNotFirst.attr("data-toggle")).hide();
+//             $(this).eq(index).addClass("active");
 
-            obj.click(function(evt){
+//             obj.click(function(evt){
 
-                toggler = "#" + obj.attr("data-toggle");
-                togglerRest = $(toggler).parent().find("div");
+//                 toggler = "#" + obj.attr("data-toggle");
+//                 togglerRest = $(toggler).parent().find("div");
 
-                togglerRest.hide().removeClass("active");
-                $(toggler).show().addClass("active");
+//                 togglerRest.hide().removeClass("active");
+//                 $(toggler).show().addClass("active");
 
-                //toggle Active Class on tab buttons
-                $(this).parent("div").find("a").removeClass("active");
-                $(this).addClass("active");
+//                 //toggle Active Class on tab buttons
+//                 $(this).parent("div").find("a").removeClass("active");
+//                 $(this).addClass("active");
 
-                return false; //Stop event Bubbling and PreventDefault
-            });
-        });
-    };
-}(jQuery));
+//                 return false; //Stop event Bubbling and PreventDefault
+//             });
+//         });
+//     };
+// }(jQuery));
 
 $(document).ready(function() {
     $(".tabs a").html5jTabs();
@@ -47,6 +47,7 @@ $(document).ready(function() {
     /*
     * CSS filters, basic implementation
     */
+    
     var filter = '';
 
     $('.filter').on('click', function(){
---
+++ public/js/tabs.js
@@ -0,0 +1,17 @@
+function TabsCtrl($scope){
+	$scope.tabs = {'stickers':true,'backgrounds':false,'filters':false,'frames':false}
+
+	$scope.show_tab = function(category){
+		console.log("category: " + category)
+		angular.forEach($scope.tabs,function(value,tab){
+			if(tab == category){
+				$scope.tabs[category] = true;
+			}
+			else{
+				$scope.tabs[tab] = false;
+			}
+			console.log(tab + ' == ' + category + " is " + (tab == category))
+			console.log('tab: ' + tab + " = " + value)
+		})
+	}
+}
