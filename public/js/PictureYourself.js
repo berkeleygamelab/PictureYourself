021300f44cdb38d2ab32d44f879a4f3cec529229
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2013-12-12 14:00:27 -0800

    Fixed hamburger

--- public/js/PictureYourself.js
+++ public/js/PictureYourself.js
@@ -48,17 +48,17 @@ function GUID(){
 
 // Hamburger drop-down
 
-$(document).ready(function(){
-	//Commented out because original code is commented out
-	//was causing an error and stopping other elements from
-	//loading.
-	
-	/*
-	var click = false;
-	var visible = false;
-	document.getElementById("dropdown").style.width = (document.getElementById("hamburger").width - parseInt(document.getElementById("dropdown").style.left)).toString + 'px';
-	// document.getElementById("dropdown").style.top = (document.getElementById("ham").style.height.slice(0, 2)).toString + 'px';
-	$("#hamburger")
+$(document).ready(function () {
+    //Commented out because original code is commented out
+    //was causing an error and stopping other elements from
+    //loading.
+
+    ///*
+    var click = false;
+    var visible = false;
+    document.getElementById("dropdown").style.width = (document.getElementById("hamburger").width - parseInt(document.getElementById("dropdown").style.left)).toString + 'px';
+    // document.getElementById("dropdown").style.top = (document.getElementById("ham").style.height.slice(0, 2)).toString + 'px';
+    $("#hamburger")
 	    .mouseover(function () {
 	        document.getElementById("dropdown").style.visibility = "visible";
 	        visible = true;
@@ -79,8 +79,8 @@ $(document).ready(function(){
 	        }
 	        click = !click;
 	    });
-	*/
-	})
+    //*/
+});
 
 function SnapshotCtrl($scope, fileReader, $http, $timeout){
 	//create proper login methods etc...
