d40f19144da81b1395b83a93f9e90e037fb6d821
Alex Liu <alex.allen.liu@gmail.com>
2015-01-07 22:52:02 -0800

    slideshow functionality done, need to style

--- public/js/slideshow/slideshow.js
+++ public/js/slideshow/slideshow.js
@@ -1,50 +1,51 @@
+// From http://www.sitepoint.com/creating-slide-show-plugin-angularjs/
 var scope;
-
-function SlideshowCtrl($scope, $http){
+app.controller('SlideshowCtrl', function($scope, $http, $interval, $timeout){
     scope = $scope;
-    $http.get('/getCollages').success(function(data){
-        console.log(data);
+    $scope.collages = null
+    // $scope.showArrows = false;
+
+    var getCollages = function(data){
         $scope.date = Date.now();
         $scope.collages = data;
+        $scope.showArrows = $($scope.collages).length > 1
+    }
+
+    // Get collages the first time
+    $http.get('/getCollages').success(getCollages);
+
+    // Update collages every 5 minutes
+    $interval(function() {
+        $http.get('/getCollages').success(getCollages);
+    }, 300000); 
+
+    $scope.currentIndex = 0; // Initially the index is at the first image
+     
+    $scope.next = function() {
+        if($($scope.collages).length > 1){
+                $scope.currentIndex < $scope.collages.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
+        }
+    };
+
+    //Update image every 5 seconds
+    $interval(function() {
+        $scope.next();
+    }, 5000); 
+
+    $scope.$watch('collages', function(){
+        $scope.$watch('currentIndex', function() {
+            if(jQuery.type($scope.collages) == "array"){
+                $scope.collages.forEach(function(collage) {
+                    collage.visible = false; // make every image invisible
+                });
+                $scope.collages[$scope.currentIndex].visible = true; // make the current image visible
+            } 
+        });
     });
 
-    // Received from LayoutCtrl
-    // $scope.$on('updateCollages', function(event, data){
-    //     console.log("Slideshow updateCollages");
-    //     $scope.collages.push(data);
-    // });
-
-    // var show = function(el){
-    //     return function(msg){ el.innerHTML = msg + '<br />' + el.innerHTML; }
-    // }(document.getElementById('msgs'));
-
-//     var ws = new WebSocket('ws://' + window.location.host + window.location.pathname);
-//     ws.onopen = function(m){      
-//         console.log("Open") 
-//     }
-//     ws.onclose = function(){
-//         console.log("Close")
-//     }
-//     ws.onmessage = function(m){
-//         console.log("Message")
-//         console.log(this);
-//         // console.log(m.data);  
-//         // $scope.$apply(function(){
-//         $scope.date = Date.now();
-//         $scope.collages = m.data;
-//         // })
-// // 
-//     }
-//     console.log($scope.date);
-//     console.log($scope.collages);
-
-    // var sender = function(f){
-    //     var input = document.getElementById('input');
-    //     input.onclick = function(){ input.value = "" };
-    //     f.onsubmit = function(){
-    //         ws.send(input.value);
-    //         input.value = "send a message";
-    //         return false;
+    // $scope.prev = function() {
+    //     if($($scope.collages).length > 1){
+    //         $scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.collages.length - 1;
     //     }
-    // }(document.getElementById('form'));
-}
+    // };
+});
