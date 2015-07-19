906bb68bacd3b46173339a6c67cd8e1007226b42
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-07-19 14:06:52 -0700

    Add routes/parse.rb to handle file upload and parsing.

--- picture_yourself.rb
+++ picture_yourself.rb
@@ -21,6 +21,7 @@ require "./models/user.rb"
 # Routes
 #--------------------
 require "./routes/user.rb"
+require "./routes/parse.rb"
 
 #------------------------------------------------------------------------------
 # Configuration
@@ -48,28 +49,6 @@ options = { :address              => "smtp.gmail.com",
 
 Mail.defaults do
   delivery_method :smtp, options
-end
-
-#------------------------------------------------------------------------------
-# handles OS detection for openCV
-#--------------------------------
-
-module OS
-  def OS.windows?
-    (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
-  end
-
-  def OS.mac?
-   (/darwin/ =~ RUBY_PLATFORM) != nil
-  end
-
-  def OS.unix?
-    !OS.windows?
-  end
-
-  def OS.linux?
-    OS.unix? and not OS.mac?
-  end
 end
 
 #------------------------------------------------------------------------------
@@ -123,44 +102,6 @@ end
 
 get "/selfie" do
   "users/#{request.cookies["pyuserid"]}/1_sticker.png"
-end
-
-#------------------------------------------------------------------------------
-# POST /fileupload
-#-----------------
-
-post '/fileupload' do
-    parsed = JSON.parse request.body.read, :symbolize_names => true
-    dirname = 'uploads/'+parsed[:name]
-    dirNumber = parsed[:count]
-    image = parsed[:data].split(',')[1]
-
-    unless File.directory?(dirname)
-      FileUtils.mkpath dirname
-    end
-
-    File.open("#{dirname}/#{dirNumber}.png", 'wb') do |f|
-      f.write(Base64.decode64(image))
-    end
-    # Needs to be updated to account for errors
-    status 200
-end
-
-post '/grabcut' do
-  parsed = JSON.parse request.body.read, :symbolize_names => true
-  filename = "#{parsed[:pyuserid]}/#{parsed[:count]}.png"
-
-  # TODO: What was the decision to separate the OpenCV libraries by OS? grabcut
-  # works on Ubuntu while opencv_trans_UNIX is always missing some .so files. For
-  # now, we'll go with grabcut for all OS.
-  # if OS.mac?
-  #   system("./grabcut uploads/#{filename} #{parsed[:coords]} #{parsed[:pyuserid]}")
-  # elsif OS.unix?
-  #   system("./opencv_trans_UNIX uploads/" + filename + ' ' + parsed[:coords] + ' ' + parsed[:pyuserid])
-  # end
-  system("./grabcut uploads/#{filename} #{parsed[:coords]} #{parsed[:pyuserid]}")
-
-  "users/#{parsed[:pyuserid]}/#{parsed[:count]}_sticker.png"
 end
 
 
