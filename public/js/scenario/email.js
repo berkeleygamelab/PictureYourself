c38926fe5e8da075979d78c7fcdf2f9bf3d00cf4
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-07-19 19:50:31 -0700

    Create routes/collage file.

--- public/js/scenario/email.js
+++ public/js/scenario/email.js
@@ -2,7 +2,7 @@
 
 // Called from scenario.js in $scope.call_email
 function email($scope, $http, formData, emails){
-    $http.post('/saveToGallery', formData).success(function(data){
+    $http.post('/collages', formData).success(function(data){
         send_email($scope, $http, emails, data);
     }).error(function(){
         $scope.show_saving_email = false;
