7d50f2155126aa575b74e257551669bce49111e2
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-10-11 18:38:50 -0700

    Display comic collage inside a table.

--- public/js/angular/controllers/comic.js
+++ public/js/angular/controllers/comic.js
@@ -1,17 +1,27 @@
 angular.module('PictureYourselfApp').controller('comicCtrl', function($scope, $timeout, $window, $http, $attrs){
   $scope.comics = angular.fromJson($attrs.comics);
 
-  var randomInteger = Math.floor(Math.random() * $scope.comics.length);
-  $scope.comic = $scope.comics[randomInteger];
-
   var sampleComic = function() {
-    $timeout(function() {
+    var randomInteger = Math.floor(Math.random() * $scope.comics.length);
+    $scope.comic = $scope.comics[randomInteger];
 
-      var randomInteger = Math.floor(Math.random() * $scope.comics.length);
-      $scope.comic = $scope.comics[randomInteger];
-      sampleComic();
+    if ($scope.comic.collages.length == 0)
+      sampleComic()
+    else {
+      $scope.comic.grouping = [];
+      for (var i = 0; i < $scope.comic.collages.length; i = i + 2 ) {
+        $scope.comic.grouping.push([ $scope.comic.collages[i], $scope.comic.collages[i+1] ])
+      }
+    }
+  }
+
+  var sampleAndQueueComic = function() {
+    sampleComic();
+
+    $timeout(function() {
+      sampleAndQueueComic();
     }, 3000);
   }
 
-  sampleComic()
+  sampleAndQueueComic();
 });
